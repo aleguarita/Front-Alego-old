739e633c28d70a6c345252707e38bcab66e35125
Alessandro Guarita <alessandro.guarita@gmail.com>
2017-01-12 11:29:19 -0200

    Ajuste na barra de ferramentas e autocomplete

--- javascripts/autocomplete.directive.js
+++ javascripts/autocomplete.directive.js
@@ -9,8 +9,15 @@
             },
             link: function (scope, elem, attr, ctrl) {
                 scope.$watch('autocompleteconfig', function (value) {
+                    
                     value.select = function(event, ui) {
                         ctrl.$setViewValue(ui.item.id);
+                    }
+
+                    value.change = function (ev, ui) {
+                        if (!ui.item)
+                            $(this).val("");
+                        }
                     }
 
                     elem.autocomplete(value);
