1bc871af7811c8cfa8357d364c8e59fcf0a5bf60
Alessandro Guarita <alessandro.guarita@gmail.com>
2017-02-10 09:45:37 -0200

    Ajustes de formulários, diretivas e JS

--- javascripts/front-alego.js
+++ javascripts/front-alego.js
@@ -1,8 +1,8 @@
 
 //= require_tree ../templates
-//= require app.js
-//= require header-tabela-mutante.directive
-//= require autocomplete.directive
-//= require menu.directive
-//= require menu-rota-ativa.directive
-//= require upload-file-name.directive
+//= require front-alego/app
+//= require front-alego/header-tabela-mutante.directive
+//= require front-alego/autocomplete.directive
+//= require front-alego/menu.directive
+//= require front-alego/menu-rota-ativa.directive
+//= require front-alego/upload-file-name.directive
